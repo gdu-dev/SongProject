7af5252f064d0394d75deaf72ae00eb7112175cd
gdu-dev <minkt78@naver.com>
2023-06-09 15:07:12 +0900

    SongProject

--- src/main/java/com/gdu/song/HomeController.java
+++ src/main/java/com/gdu/song/HomeController.java
@@ -1,12 +1,12 @@
 package com.gdu.song;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping("/")
+	@PostMapping("/")
 	public String index() {
 		return "index";
 	}
